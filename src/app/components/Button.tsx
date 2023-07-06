baeb28f9841124fec74edbee947b1544dbb0db8c
Carlos <iamcarllosjr@gmail.com>
2023-07-05 22:00:09 -0300

    Erro in prettier

--- src/app/components/Button.tsx
+++ src/app/components/Button.tsx
@@ -1,12 +1,12 @@
-import useColorMode from "@/app/components/useColorMode";
-import { Moon, Sun } from "lucide-react";
+import useColorMode from '@/app/components/useColorMode'
+import { Moon, Sun } from 'lucide-react'
 
 const Button = () => {
   const [colorMode, setColorMode] = useColorMode();
  
   return (
     <button onClick={() => setColorMode(colorMode === 'light' ? 'dark' : 'light')}>
-        {colorMode === 'light' ? <Sun className="hover:text-rose-600 hover:scale-110"/> : <Moon className="hover:text-rose-600 hover:scale-110"/>}
+        {colorMode === 'light' ? <Sun className='hover:text-rose-600 hover:scale-110'/> : <Moon className='hover:text-rose-600 hover:scale-110'/>}
     </button>
   )
 
