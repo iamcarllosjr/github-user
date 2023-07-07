951f4f05256dd2e883d8a080d3c65cd953fcf04f
Carlos <iamcarllosjr@gmail.com>
2023-07-06 21:39:39 -0300

    modified layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import "@/styles/global.css";
 import { Metadata } from "next";
 import { ReactNode } from "react";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -14,7 +17,7 @@ interface RootLayoutProps {
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`dark ${inter.className}`}>{children}</body>
     </html>
   );
 }
